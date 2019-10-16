3fc3c5f1b4369e8cdf6971e50bbb446cca88c766
Esteban <esteban@digidip.net>
2019-10-16 16:53:37 +0200

    Ejemplo practico de componente mensaje usando bulma css

---
+++ messageComponent.js
@@ -0,0 +1,35 @@
+Vue.component('message', {
+    props: ['title', 'body'],
+
+    template: `
+        <article class="message" v-show="isVisible">
+            <div class="message-header">
+                <p>{{ title }}</p>
+                <button class="delete" aria-label="delete" @click="hideModal"></button>
+            </div>
+            <div class="message-body">
+                {{ body }}
+            </div>
+        </article>
+    `,
+
+    data() {
+        return {
+            isVisible: true
+        };
+    },
+
+    methods: {
+        hideModal() {
+            this.isVisible = false;
+        }
+    }
+});
+
+Vue.component('task', {
+    template: '<li><slot></slot></li>'
+});
+
+new Vue({
+    el: '#root'
+});
